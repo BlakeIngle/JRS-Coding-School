1e1d4cc7efe54e3d3fb7e0930f7e2b02fcb65e6e
BlakeIngle <blake.ingle7@gmail.com>
2021-11-18 09:58:42 -0500

    added merge array exercise

---
+++ js/sort/mergeArrays.js
@@ -0,0 +1,16 @@
+var arr1 = [0, 1, 1, 2, 3, 4, 5, 5, 6, 7, 8];
+var arr2 = [0, 0, 1, 3, 3, 4, 5, 7, 7, 8, 9];
+
+
+function mergeArrays(arr1, arr2) {
+    var mergedArr = [];
+    /* -------------------------------------------- */
+    // write a function that will return an array
+    // where the values in that array are all the 
+    // values from two given arrays (in sorted order)
+
+    /* -------------------------------------------- */
+    return mergedArr;
+}
+
+console.log(mergeArrays(arr1, arr2))
