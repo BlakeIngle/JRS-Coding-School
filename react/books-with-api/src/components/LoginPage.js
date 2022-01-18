f1d0fcfc4bf817a363726a8059222e1aeb52aa30
BlakeIngle <blake.ingle7@gmail.com>
2022-01-18 15:25:14 -0500

    animated the login form on error

--- react/books-with-api/src/components/LoginPage.js
+++ react/books-with-api/src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router';
 import './login.css';
 
@@ -7,30 +7,11 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
 
-    var { http, state, setState, localStorageService } = useContext(Context);
-
-    const navigate = useNavigate();
-
-    function attemptLogin(user) {
-        // do something
-        http.login(user)
-            .then(res => {
-                // 200 -> login successful
-                console.log(res.data.user)
-                localStorageService.saveUser(res.data.user);
-                navigate(`/user/${res.data.user.id}`);
-            }).catch(err => {
-                console.error(err)
-                // maybe a 404 -> bad email
-                // maybe 4xx -> good email, bad password
-            });
-    }
-
     return (
         <div className="login">
             <br />
             <br />
-            <LoginForm onSubmit={attemptLogin} />
+            <LoginForm />
             <hr />
             <Link to="/signup">
                 <button type="button">Sign Up</button>
@@ -39,12 +20,19 @@ export default function LoginPage() {
     )
 }
 
-function LoginForm({ onSubmit }) {
+function LoginForm() {
+
+    var { http, state, setState, localStorageService } = useContext(Context);
+    const navigate = useNavigate();
+    const animationTime = 300;
 
     const [user, setUser] = useState({
         email: '',
         password: ''
     });
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
     function handleChange(e) {
         var name = e.target.name;
@@ -59,7 +47,27 @@ function LoginForm({ onSubmit }) {
     function handleSubmit(e) {
         e.preventDefault();
         if (user.email && user.password) {
-            onSubmit(user);
+
+            // do something
+            http.login(user)
+                .then(res => {
+                    // 200 -> login successful
+                    console.log(res.data.user)
+                    localStorageService.saveUser(res.data.user);
+                    navigate(`/user/${res.data.user.id}`);
+                }).catch(err => {
+                    console.error(err)
+
+                    emailRef.current.classList.add("shake");
+                    passwordRef.current.classList.add("shake");
+
+                    setUser({ email: '', password: '' });
+
+                    setTimeout(() => {
+                        emailRef.current.classList.remove("shake");
+                        passwordRef.current.classList.remove("shake");
+                    }, animationTime);
+                });
         }
     }
 
@@ -67,15 +75,19 @@ function LoginForm({ onSubmit }) {
         <form onSubmit={handleSubmit}>
             <label>Email:</label>
             <input type="text"
+                ref={emailRef}
                 name="email"
                 value={user.email}
+                style={{ "--animationTime": `${animationTime}ms` }}
                 onChange={handleChange} />
             <br />
 
             <label>Password:</label>
             <input type="password"
+                ref={passwordRef}
                 name="password"
                 value={user.password}
+                style={{ "--animationTime": `${animationTime}ms` }}
                 onChange={handleChange} />
             <br />
 
