eafdece9357d6e025263b43959526d9b93a9c26b
Blake Ingle <blake.ingle7@gmail.com>
2022-04-19 18:48:29 -0400

    added use reducer project

---
+++ react/10-use-reducer/src/assets/reducer.js
@@ -0,0 +1,19 @@
+/**
+ * Given the current state and some action, do anything.
+ * Must return a value of the new state.
+ * 
+ * Multiple actions can be condensed or 'reduced' into one 
+ * function this way.
+ * 
+ * The action must be an object with a 'type' prop
+ * and an optional 'payload' prop
+ * 
+ * The reducer function includes all 'buisness logic' so the
+ * components do not get too bulky
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
+export function reducer (state, action) {
+    return state;
+}
